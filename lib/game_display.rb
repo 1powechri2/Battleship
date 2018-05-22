ebce40e7b4979e95c27ffd916ce9f540abea480a
Chris Powell <chris.ian.powell@gmail.com>
2018-05-22 12:54:17 -0600

    added map for display grid

--- lib/game_display.rb
+++ lib/game_display.rb
@@ -1,10 +1,17 @@
 class GameDisplay
+  attr_reader :rows,
+              :display_positions
+
   def initialize
     @header = '.A B C D'
     @rows   = ['1       ',\
                '2       ',\
                '3       ',\
                '4       ']
+    @display_positions = {a1:[0, 1], a2:[0, 3], a3:[0, 5], a4:[0, 7],\
+                          b1:[1, 1], b2:[1, 3], b3:[1, 5], b4:[1, 7],\
+                          c1:[2, 1], c2:[2, 3], c3:[2, 5], c4:[2, 7],\
+                          d1:[3, 1], d2:[3, 3], d3:[3, 5], d4:[3, 7]}
   end
 
   def print_to_screen
