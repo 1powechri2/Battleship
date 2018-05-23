83b17e935856e9cbbf26fb71a38b40412061b740
Chris Powell <chris.ian.powell@gmail.com>
2018-05-23 11:35:25 -0600

    saving changes to runner

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -2,6 +2,7 @@ require './lib/game_board'
 require './lib/game_display'
 require './lib/battleship_methods'
 require './lib/messages'
+require 'pry'
 
 class Battleship
   include Messages
@@ -12,14 +13,18 @@ class Battleship
     @player_display   = GameDisplay.new
     @computer_display = GameDisplay.new
     @game = BattleshipMethods.new(@player_board, @computer_board,\
-                             @computer_display, @player_display)
+                                  @computer_display, @player_display)
+    @comp_ship_size_2 = nil
+    @comp_ship_size_3 = nil
+    @play_ship_size_2 = nil
+    @play_ship_size_3 = nil
   end
 
   def welcome
     welcome_message
     welcome = gets.chomp
     if welcome.downcase == 'p'
-      setup_game
+      setup_computer
     elsif welcome.downcase == 'i'
       instructions
     elsif welcom.downcase == 'q'
@@ -27,31 +32,69 @@ class Battleship
     end
   end
 
-  def setup_game
-    # SET COMPUTER SHIPS AND PLACE
-    ship_size_2 = @computer_board.pick_ship_placement_size_2_for_computer
-    no_overlap  = @computer_board.ship_size_2_will_not_overlap_ship_size_3(ship_size_2)
-    ship_size_3 = @computer_board.pick_ship_placement_size_3_for_computer(no_overlap)
+  def setup_computer
+    @comp_ship_size_2 = @computer_board.pick_ship_placement_size_2_for_computer
+    no_overlap  = @computer_board.ship_size_2_will_not_overlap_ship_size_3(@comp_ship_size_2)
+    @comp_ship_size_3 = @computer_board.pick_ship_placement_size_3_for_computer(no_overlap)
 
-    @computer_board.place_ship(ship_size_2)
-    @computer_board.place_ship(ship_size_3)
-    # SET PLAYER SHIPS AND PLACE
+    @computer_board.place_ship(@comp_ship_size_2)
+    @computer_board.place_ship(@comp_ship_size_3)
+    setup_player
+  end
+
+  def setup_player
     pick_ship_size_2_message
     player_ship_size_2 = gets.chomp
-    p_ship_size_2 = @player_board.pick_ship_placement_size_2_for_human(player_ship_size_2)
+    @play_ship_size_2 = @player_board.pick_ship_placement_size_2_for_human(player_ship_size_2)
     pick_ship_size_3_message
     player_ship_size_3 = gets.chomp
-    no_overlap = @player_board.ship_size_2_will_not_overlap_ship_size_3(p_ship_size_2)
-    p_ship_size_3 = @player_board.pick_ship_placement_size_3_for_human(no_overlap, player_ship_size_3)
+    no_overlap = @player_board.ship_size_2_will_not_overlap_ship_size_3(@play_ship_size_2)
+    @play_ship_size_3 = @player_board.pick_ship_placement_size_3_for_human(no_overlap, player_ship_size_3)
 
-    @game.humanoid_game_display_records_ships(p_ship_size_2)
-    @game.humanoid_game_display_records_ships(p_ship_size_3)
+    @game.humanoid_game_display_records_ships(@play_ship_size_2)
+    @game.humanoid_game_display_records_ships(@play_ship_size_3)
 
-    puts ' PLAYER'
-    puts "#{@game.humanoid_game_display.print_to_screen}"
-    puts 'COMPUTER'
-    puts "#{@game.computer_game_display.print_to_screen}"
+    game_loop
   end
+
+  def game_loop
+    loop do
+      puts ' PLAYER'
+      puts "#{@game.humanoid_game_display.print_to_screen}"
+      puts 'COMPUTER'
+      puts "#{@game.computer_game_display.print_to_screen}"
+
+      print 'Player Fire!!! >'
+      guess = gets.chomp
+      shot_position = @game.player_retrieve_grid_position(guess)
+      hit = @game.computer_battleship_hit?(shot)
+      sunk_1 = @game.player_battleship_sunk?(@comp_ship_size_2)
+      sunk_2 = @game.player_battleship_sunk?(@comp_ship_size_3)
+      if hit == true
+        puts "You've hit my battleship!!!"
+        @game.mark_hit_computer_ship(hit, shot_position)
+      else
+        puts "You've Missed"
+      end
+
+      if sunk_1 == true
+        puts "You've sunk my battleship!!!"
+      end
+
+      if sunk_2 == true
+        puts "You've sunk my battleship!!!"
+      end
+
+      if @game.count_computer_hits == 5
+        puts "You're Awesome"
+        break
+      elsif @game.count_player_hits == 5
+        puts "You Suck"
+        break
+      end
+    end
+  end
+
 end
 
 play_battleship = Battleship.new
